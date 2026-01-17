a1cc1ba47eed10d633c94d13d47c4f7e11d7a55a
YeDemirkiran <yedemirkiran27@gmail.com>
2026-01-17 15:22:32 +0300

    Add kernel.c

---
+++ kernel.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+
+volatile uint16_t	*vga = (uint16_t *)0xB8000;
+
+void	print(const char *str)
+{
+	uint16_t	pos;
+
+	pos = 0;
+	while (str[pos] != '\0')
+	{
+		vga[pos] = (uint16_t)str[pos] | 0x0F00;
+		pos++;
+	}
+}
+
+void	kernel_main(void)
+{
+	print("Anan");
+
+	while (1)
+	{
+		__asm__ volatile ("hlt");
+	}
+}
